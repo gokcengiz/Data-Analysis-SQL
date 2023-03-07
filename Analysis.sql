76d253ed611cdd5fbdca9c4b006c3b2c5e6f4f30
gokcen <gokcencngz@gmail.com>
2023-03-08 00:11:52 +0300

    First query

---
+++ Analysis.sql
@@ -0,0 +1,27 @@
+---En yüksek rating değerlendirmeli 'Data Analysis' kurslarında ilk 10?
+SELECT title,rating FROM courses
+WHERE title ILIKE '%Data Analysis%'
+ORDER BY rating DESC
+LIMIT 10;
+
+---Max rating değerinde kaç kurs vardır?
+SELECT count(rating) FROM courses
+WHERE rating = (SELECT MAX (rating) FROM courses);
+
+---En fazla değerlendirilen kurs hangisidir?
+SELECT title, num_reviews FROM courses
+ORDER BY num_reviews DESC
+LIMIT 1;
+
+---En çok değerlendirilen 10 kurs?
+SELECT title, num_reviews from courses
+ORDER BY num_reviews DESC
+LIMIT 10;
+
+---Rating ve değerlendirme sayısı en yüksek olan kurslar
+SELECT title, rating, num_reviews FROM courses
+ORDER BY num_reviews DESC;
+
+---Kurslardaki ortalama ders sayısı
+SELECT avg(num_published_lectures) FROM courses;
+
