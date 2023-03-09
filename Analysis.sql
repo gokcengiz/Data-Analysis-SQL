47175867d62608897e61c805eea83e81f9272702
gokcen <gokcencngz@gmail.com>
2023-03-09 15:12:14 +0300

    Query added

--- Analysis.sql
+++ Analysis.sql
@@ -18,8 +18,11 @@ SELECT title, num_reviews from courses
 ORDER BY num_reviews DESC
 LIMIT 10;
 
----Rating ve değerlendirme sayısı en yüksek olan kurslar
-SELECT title, rating, num_reviews FROM courses;
+---Değerlendirme sayısı en yüksek ve değerlendirmesi ortalamanın üstünde olan kurslar? / EN POPÜLER 10 KURS
+SELECT title, rating, num_reviews FROM courses
+WHERE rating >= (SELECT avg(rating) FROM courses)
+ORDER BY num_reviews DESC
+LIMIT 10;
 
 
 ---Kurslardaki ortalama ders sayısı
@@ -32,9 +35,47 @@ FROM courses c
 INNER JOIN instructors i
 ON i.id = c.instructors_id;
 
+
 ---Her bir eğitmenin kaç eğitim videosu vardır?
 SELECT instructors_id,
 count(title) FROM courses
 GROUP BY instructors_id
 ORDER BY count(title) DESC;
 
+
+---Kursların değerlendirme ortalamaları kaçtır?
+SELECT avg(rating) FROM courses;
+
+
+---Kaç tane data analyst eğitmen var?
+SELECT count(job_title) FROM instructors
+WHERE job_title ILIKE '%Data Analyst%';
+
+
+---En fazla değerlendirilen 10 eğitmen / EN POPüLER 10 EĞİTMEN
+SELECT i.title, c.num_reviews
+FROM courses c
+INNER JOIN instructors i
+ON i.id = c.instructors_id
+ORDER BY num_reviews DESC
+LIMIT 10;
+
+---En fazla değerlendirilen eğitmenin değerlendirme sıralamalı tüm kursları
+SELECT title,rating FROM courses
+WHERE instructors_id = (
+SELECT i.id
+FROM courses c
+INNER JOIN instructors i
+ON i.id = c.instructors_id
+ORDER BY num_reviews DESC
+LIMIT 1)
+ORDER BY rating DESC;
+
+
+---En fazla değerlendirilen eğitmenin id si
+SELECT i.id
+FROM courses c
+INNER JOIN instructors i
+ON i.id = c.instructors_id
+ORDER BY num_reviews DESC
+LIMIT 1;
